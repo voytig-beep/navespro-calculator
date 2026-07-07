3a4d494147ea64a2dea21cf069cf6c10f20e0bee
Querido Amigo <querido-amigo@users.noreply.github.com>
2026-07-07 14:39:47 +0300

    Deploy site to GitHub Pages

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  base: command === "build" ? "/navespro-calculator/" : "/",
   optimizeDeps: {
     include: ["react", "react-dom/client"],
   },
@@ -11,4 +12,4 @@ export default defineConfig({
     },
   },
   plugins: [react()],
-});
+}));
